04440c8c99b6a05278592cfc8363beca1bbb6f7d
mohamed_ali <midos123rrr@gmail.com>
2025-09-12 23:02:37 +0300

    best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/futured_list_view.dart';
@@ -17,9 +18,32 @@ class HomeViewBody extends StatelessWidget {
           SizedBox(height: 50),
           const FutureBooksListView(),
           SizedBox(height: 50),
-
           Text('Best Seller', style: Styles.titleMedium),
+          SizedBox(height: 20),
+          BestSellerListViewItem(),
         ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage(AssetsData.book1),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
       ),
     );
   }
