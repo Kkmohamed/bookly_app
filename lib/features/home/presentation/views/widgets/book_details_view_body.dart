1a30c0996cab5e4bf631b90e789a4cbf31b6e1e5
mohamed_ali <midos123rrr@gmail.com>
2025-09-15 22:31:11 +0300

    create similar books list view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -4,6 +4,7 @@ import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart
 import 'package:bookly/features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/future_list_view_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -16,30 +17,47 @@ class BookDetailsViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 28.0),
       child: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 12),
-            const CustomBookDetailsAppBar(),
-            SizedBox(height: 30),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-              child: CustomBookImage(),
-            ),
-            SizedBox(height: 40),
-            Text('The Jungle Book', style: Styles.textStyle30),
-            SizedBox(height: 6),
-            Opacity(
-              opacity: 0.7,
-              child: Text(
-                'Rudyard Kipling',
-                style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 12),
+              const CustomBookDetailsAppBar(),
+              SizedBox(height: 30),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+                child: CustomBookImage(),
               ),
-            ),
-            SizedBox(height: 16),
-            BookRating(mainAxisAlignment: MainAxisAlignment.center),
-            SizedBox(height: 37),
-            BooksAction(),
-          ],
+              SizedBox(height: 40),
+              Text('The Jungle Book', style: Styles.textStyle30),
+              SizedBox(height: 6),
+              Opacity(
+                opacity: 0.7,
+                child: Text(
+                  'Rudyard Kipling',
+                  style: Styles.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              BookRating(mainAxisAlignment: MainAxisAlignment.center),
+              SizedBox(height: 37),
+              BooksAction(),
+              SizedBox(height: 50),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'You can also like',
+                  style: Styles.textStyle18.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              SimilarBooksListview(),
+              SizedBox(height: 40),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -0,0 +1,24 @@
+
+import 'package:bookly/features/home/presentation/views/widgets/future_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class SimilarBooksListview extends StatelessWidget {
+  const SimilarBooksListview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.16,
+      child: ListView.builder(
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: const CustomBookImage(),
+          );
+        },
+      ),
+    );
+  }
+}
